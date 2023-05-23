b7f1ce802a36ed313dae051d976ffa7a69a00ad5
stephen842 <ugotachisomstephen@gmail.com>
2023-05-23 12:05:47 -0700

    hot ficx

--- SS_main.c
+++ SS_main.c
@@ -10,36 +10,36 @@
 
 int main(int ac, char **av)
 {
-        info_t info[] = { INFO_INIT };
-        int b = 2;
+	info_t info[] = { INFO_INIT };
+	int b = 2;
 
-        asm ("mov %1, %0\n\t"
-                        "add $3, %0"
-                        : "=r" (b)
-                        : "r" (b));
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (b)
+			: "r" (b));
 
-        if (ac == 2)
-        {
-                b = open(av[1], O_RDONLY);
-                if (b == -1)
-                {
-                        if (errno == EACCES)
-                                exit(126);
-                        if (errno == ENOENT)
-                        {
-                                _eputchar('\n');
-                                _eputchar(BUF_FLUSH);
-                                _eputs(av[0]);
-                                _eputs(": 0: Can't open ");
-                                _eputs(av[1]);
-                                exit(127);
-                        }
-                        return (EXIT_FAILURE);
-                }
-                info->readfd = b;
-        }
-        populate_env_list(info);
-        read_history(info);
-        hsh(info, av);
-        return (EXIT_SUCCESS);
+	if (ac == 2)
+	{
+		b = open(av[1], O_RDONLY);
+		if (b == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = b;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
